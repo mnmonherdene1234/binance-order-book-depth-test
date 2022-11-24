4cba8880408b3e588eef0dd6474fac55f5549811
mnmonherdene <mnmonherdene1234@gmail.com>
2022-11-24 23:39:21 +0800

    today done

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,28 @@
 import { Order } from "./dto/order.dto";
 
 async function averageCalculator() {
-  const orders = await fetchOrders("MATICUSDT", 100);
-  const bidsAveragePrice =
-    orders.bids.reduce((prev, cur) => (prev += cur.price), 0) /
-    orders.bids.length;
-  const asksAveragePrice =
-    orders.asks.reduce((prev, cur) => (prev += cur.price), 0) /
-    orders.asks.length;
+  const { bids, asks } = await fetchOrders("MATICUSDT", 500);
+  const bidsAveragePrice = averagePrice(bids);
+  const asksAveragePrice = averagePrice(asks);
+
+  console.log(Math.max(...bids.map((bid) => bid.price)));
+  console.log(Math.min(...bids.map((bid) => bid.price)));
 
   console.log("AVERAGE PRICE");
   if (bidsAveragePrice > asksAveragePrice) {
     console.log(`BIDS: ${bidsAveragePrice} > ASKS: ${asksAveragePrice}`);
   } else {
     console.log(`BIDS: ${bidsAveragePrice} < ASKS: ${asksAveragePrice}`);
+  }
+
+  const bidsAverageAmount = averageAmount(bids);
+  const asksAverageAmount = averageAmount(asks);
+
+  console.log("AVERAGE AMOUNT");
+  if (bidsAverageAmount > asksAverageAmount) {
+    console.log(`BIDS: ${bidsAverageAmount} > ASKS: ${asksAverageAmount}`);
+  } else {
+    console.log(`BIDS: ${bidsAverageAmount} < ASKS: ${asksAverageAmount}`);
   }
 }
 
@@ -27,13 +36,13 @@ async function fetchOrders(pair: string, limit: number) {
   const bids: Order[] = [];
   for (let i = 0; i < data.bids.length; i++) {
     const [price, amount] = data.bids[i];
-    bids.push(new Order(price, amount));
+    bids.push(new Order(+price, +amount));
   }
 
   const asks: Order[] = [];
   for (let i = 0; i < data.asks.length; i++) {
     const [price, amount] = data.asks[i];
-    asks.push(new Order(price, amount));
+    asks.push(new Order(+price, +amount));
   }
 
   return {
